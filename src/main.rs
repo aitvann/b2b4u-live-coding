25039a083d2d2a830b272274f59f831f098aebf6
Ivan Aytzhanov <aitvann@gmail.com>
2026-07-06 22:37:42 +0300

    feat: second attempt

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
-use std::{
-    net::{IpAddr, SocketAddr},
-    sync::{
-        Arc, Mutex,
-        atomic::{AtomicU64, Ordering},
-    },
-};
-
-use dashmap::DashMap;
+use std::sync::{Arc, Mutex};
+use std::{fmt, net::SocketAddr};
 
 // Задача: написать библиотеку балансировщика нагрузки с одним алгоритмом балансировки на выбор и возможностью расширения новыми алгоритмами. Должны быть поддержаны три основные функции: добавил узел в список для балансирования, удалить узел и получить следующий узел по алгоритму балансировки.
 //
@@ -17,74 +10,86 @@ use dashmap::DashMap;
 // 4. Должна быть возможность менять стратегию балансировки в рантайме.
 // 5. Модель узла не имеет значения, достаточно идентификатора
 
-type Key = u64;
-
-#[derive(Default, Debug)]
-struct RoundRobinStrategy {
-    last_key: Option<Key>,
+pub trait Strategy: fmt::Debug {
+    fn route<'a>(&mut self, nodes: &'a [Node]) -> &'a Node;
 }
 
 #[derive(Default, Debug)]
-struct RandomStrategy;
-
-#[derive(Debug)]
-enum Strategy {
-    RoundRobin(RoundRobinStrategy),
-    Random(RandomStrategy),
+pub struct RoundRobin {
+    current_node_idx: usize,
 }
 
-#[derive(Debug)]
-struct Node {
-    key: u64,
+impl Strategy for RoundRobin {
+    fn route<'a>(&mut self, nodes: &'a [Node]) -> &'a Node {
+        if self.current_node_idx >= nodes.len() {
+            self.current_node_idx = 0;
+        }
+
+        let node = &nodes[self.current_node_idx];
+        self.current_node_idx += 1;
+        node
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Clone, Debug)]
+pub struct Node {
     addr: SocketAddr,
 }
 
 #[derive(Debug)]
-struct Balancer {
-    key_generator: AtomicU64,
-    strategy: Mutex<Strategy>,
-    nodes: DashMap<Key, Node>,
+pub struct Balancer {
+    strategy: Mutex<Box<dyn Strategy>>,
+    nodes: Mutex<Vec<Node>>,
 }
 
 impl Balancer {
-    fn new(strategy: Strategy) -> Self {
+    pub fn new(strategy: Box<dyn Strategy>) -> Self {
         Self {
-            key_generator: AtomicU64::new(0),
             strategy: Mutex::new(strategy),
-            nodes: Default::default(),
+            nodes: Mutex::new(vec![]),
         }
     }
 
-    fn gen_key(&self) -> u64 {
-        self.key_generator.fetch_add(1, Ordering::SeqCst)
+    pub fn add_node(&self, node: Node) {
+        let mut guard = self.nodes.lock().unwrap();
+        guard.push(node);
     }
 
-    fn set_stragety(&self, strategy: Strategy) {
-        //
+    pub fn pick_node(&self) -> Node {
+        let mut strategy = self.strategy.lock().unwrap();
+        let nodes = self.nodes.lock().unwrap();
+        let node = strategy.route(&nodes);
+        node.clone()
     }
 
-    fn add_node(&self, node: Node) {
-        if let Some(prev) = self.nodes.insert(node.key, node) {
-            println!("Node override: {}", prev.key);
-        }
-    }
-
-    fn pick_node(&self) -> Node {
-        todo!()
+    #[allow(dead_code)]
+    pub fn set_strategy(&self, strategy: Box<dyn Strategy>) {
+        *self.strategy.lock().unwrap() = strategy;
     }
 }
 
 fn main() {
-    let strategy = Strategy::RoundRobin(RoundRobinStrategy::default());
-    let balancer = Balancer::new(strategy);
-    let balancer = Arc::new(balancer);
+    let strategy = RoundRobin::default();
+    let balancer = Balancer::new(Box::new(strategy));
+    // let balancer = Arc::new(balancer);
 
-    let new_id = balancer.gen_key();
-    let node = Node {
-        key: new_id,
+    let node1 = Node {
         addr: "127.0.0.1:1234".parse().unwrap(),
     };
-    balancer.add_node(node);
+    balancer.add_node(node1);
 
-    println!("Hello, world!");
+    let node2 = Node {
+        addr: "127.0.0.1:4567".parse().unwrap(),
+    };
+    balancer.add_node(node2);
+
+    dbg!(balancer.pick_node());
+    dbg!(balancer.pick_node());
+    dbg!(balancer.pick_node());
+    dbg!(balancer.pick_node());
+    dbg!(balancer.pick_node());
+    dbg!(balancer.pick_node());
+    dbg!(balancer.pick_node());
+    dbg!(balancer.pick_node());
 }
